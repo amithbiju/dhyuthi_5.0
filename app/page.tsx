01e9f88445afa5d7815db14769e348c15e7925e0
Amith Biju <amithbiju1@gmail.com>
2024-09-21 12:06:38 +0530

    image_removed

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Hero from "@/components/Hero";
 import Event from "@/components/Event";
 import { Timer } from "@/components/Timer";
 import Venue from "@/components/Venue";
-import Image from "next/image";
 
 export default function Home() {
   return (
