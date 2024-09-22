bd66ac7584941dfa9f0302e6205a57ab70599b41
Amith Biju <amithbiju1@gmail.com>
2024-09-22 23:12:45 +0530

    shedule_up

--- components/Event.tsx
+++ components/Event.tsx
@@ -1,106 +1,14 @@
 "use client";
+import Day1 from "@/components/util/Day1";
 import React, { useState } from "react";
+import Day2 from "./util/Day2";
+import Day3 from "./util/Day3";
 
-// Dummy components for Profile, Dashboard, and Settings
-const Profile = () => (
-  <div>
-    <ol className="relative border-s border-gray-200 dark:border-gray-700">
-      <li className="mb-10 ms-6">
-        <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-          <svg
-            className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-          >
-            <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-          </svg>
-        </span>
-        <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-          Flowbite Application UI v2.0.0{" "}
-          <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ms-3">
-            Latest
-          </span>
-        </h3>
-        <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-          Released on January 13th, 2022
-        </time>
-        <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-          Get access to over 20+ pages including a dashboard layout, charts,
-          kanban board, calendar, and pre-order E-commerce & Marketing pages.
-        </p>
-        <a
-          href="#"
-          className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-        >
-          <svg
-            className="w-3.5 h-3.5 me-2.5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-          >
-            <path d="M14.707 7.793a1 1 0 0 0-1.414 0L11 10.086V1.5a1 1 0 0 0-2 0v8.586L6.707 7.793a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.416 0l4-4a1 1 0 0 0-.002-1.414Z" />
-            <path d="M18 12h-2.55l-2.975 2.975a3.5 3.5 0 0 1-4.95 0L4.55 12H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2Zm-3 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z" />
-          </svg>{" "}
-          Download ZIP
-        </a>
-      </li>
-      <li className="mb-10 ms-6">
-        <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-          <svg
-            className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-          >
-            <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-          </svg>
-        </span>
-        <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-          Flowbite Figma v1.3.0
-        </h3>
-        <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-          Released on December 7th, 2021
-        </time>
-        <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-          All of the pages and components are first designed in Figma and we
-          keep a parity between the two versions even as we update the project.
-        </p>
-      </li>
-      <li className="ms-6">
-        <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-          <svg
-            className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-          >
-            <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-          </svg>
-        </span>
-        <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-          Flowbite Library v1.2.2
-        </h3>
-        <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-          Released on December 2nd, 2021
-        </time>
-        <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-          Get started with dozens of web components and interactive elements
-          built on top of Tailwind CSS.
-        </p>
-      </li>
-    </ol>
-  </div>
-);
 const Dashboard = () => <div>Dashboard Content</div>;
 const Settings = () => <div>Settings Content</div>;
 
 const Event = () => {
-  const [activeTab, setActiveTab] = useState("profile"); // Default to "profile"
+  const [activeTab, setActiveTab] = useState("day1"); // Default to "profile"
 
   // Function to handle tab change
   const handleTabChange = (tab: string) => {
@@ -108,52 +16,57 @@ const Event = () => {
   };
 
   return (
-    <div className="md:flex m-10 md:ml-48">
-      {/* Sidebar Navigation */}
-      <ul className="flex-column space-y space-y-4 text-sm font-medium text-gray-500 dark:text-gray-400 md:me-4 mb-4 md:mb-0">
-        <li>
-          <button
-            onClick={() => handleTabChange("profile")}
-            className={`inline-flex items-center px-4 py-3 w-full rounded-lg ${
-              activeTab === "profile"
-                ? "text-white bg-gradient-to-r from-[#efb98f] via-[#ef9a06] to-[#f1871d] "
-                : "bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100"
-            }`}
-          >
-            DAY 1
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => handleTabChange("dashboard")}
-            className={`inline-flex items-center px-4 py-3 w-full rounded-lg ${
-              activeTab === "dashboard"
-                ? "text-white bg-gradient-to-r from-[#efb98f] via-[#ef9a06] to-[#f1871d]"
-                : "bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100"
-            }`}
-          >
-            DAY 2
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => handleTabChange("settings")}
-            className={`inline-flex items-center px-4 py-3 w-full rounded-lg ${
-              activeTab === "settings"
-                ? "text-white bg-gradient-to-r from-[#efb98f] via-[#ef9a06] to-[#f1871d]"
-                : "bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100"
-            }`}
-          >
-            DAY 3
-          </button>
-        </li>
-      </ul>
+    <div>
+      <h2 className="md:flex m-10 md:ml-48 font-palanquin text-4xl mb-10 font-bold lg:max-w-lg lg:text-start ">
+        Schedule
+      </h2>
+      <div className="md:flex m-10 md:ml-48">
+        {/* Sidebar Navigation */}
+        <ul className="flex-column space-y space-y-4 text-sm font-medium text-gray-500 dark:text-gray-400 md:me-4 mb-4 md:mb-0">
+          <li>
+            <button
+              onClick={() => handleTabChange("day1")}
+              className={`inline-flex items-center px-4 py-3 w-full rounded-lg ${
+                activeTab === "day1"
+                  ? "text-white bg-gradient-to-r from-[#efb98f] via-[#ef9a06] to-[#f1871d] "
+                  : "bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100"
+              }`}
+            >
+              DAY 1
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => handleTabChange("day2")}
+              className={`inline-flex items-center px-4 py-3 w-full rounded-lg ${
+                activeTab === "day2"
+                  ? "text-white bg-gradient-to-r from-[#efb98f] via-[#ef9a06] to-[#f1871d]"
+                  : "bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100"
+              }`}
+            >
+              DAY 2
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => handleTabChange("day3")}
+              className={`inline-flex items-center px-4 py-3 w-full rounded-lg ${
+                activeTab === "day3"
+                  ? "text-white bg-gradient-to-r from-[#efb98f] via-[#ef9a06] to-[#f1871d]"
+                  : "bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100"
+              }`}
+            >
+              DAY 3
+            </button>
+          </li>
+        </ul>
 
-      {/* Main content */}
-      <div className="pl-12 bg-transparent text-medium text-gray-500  rounded-lg w-full">
-        {activeTab === "profile" && <Profile />}
-        {activeTab === "dashboard" && <Dashboard />}
-        {activeTab === "settings" && <Settings />}
+        {/* Main content */}
+        <div className="pl-12 bg-transparent text-medium text-gray-500  rounded-lg w-full">
+          {activeTab === "day1" && <Day1 />}
+          {activeTab === "day2" && <Day2 />}
+          {activeTab === "day3" && <Day3 />}
+        </div>
       </div>
     </div>
   );
