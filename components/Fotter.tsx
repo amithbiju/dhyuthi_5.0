d7d9035df7041ffbf10cbbe65f26187f6ecef79d
Amith Biju <amithbiju1@gmail.com>
2024-09-24 22:49:11 +0530

    removed

--- components/Fotter.tsx
+++ components/Fotter.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Image from "next/image";
 import logo from "../assets/images/logo.png";
-import { Link } from "react-scroll";
 
 export const Fotter = () => {
   return (
